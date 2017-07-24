5fe4caec950202a4bc870f48150f870c09276cd2
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:33:26 +0200

    variables: Add needs_text_relocations product variable
    Change-Id: I257dfe51be7e771611dd57ae57f073ab6fe6d392
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -73,6 +73,10 @@ type variableProperties struct {
 			Cflags []string
 		}
 
+		Needs_text_relocations struct {
+			Cppflags []string
+		}
+
 		Uses_media_extensions struct {
 			Cflags []string
 		}
@@ -156,6 +160,7 @@ type productVariables struct {
 	Libart_img_base            *string `json:",omitempty"`
 	Has_legacy_camera_hal1     *bool `json:",omitempty"`
 	Uses_media_extensions      *bool `json:",omitempty"`
+	Needs_text_relocations     *bool `json:",omitempty"`
 
 	IntegerOverflowExcludePaths *[]string `json:",omitempty"`
 
