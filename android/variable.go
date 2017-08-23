808a219e2aa0677311ec3a97fd5b37fb9a6a7ca8
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    [build/soong]: extend with pure board flags
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"pure/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -94,6 +96,9 @@ type variableProperties struct {
 		Pdk struct {
 			Enabled *bool
 		}
+
+		// include Pure variables
+		*android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -172,6 +177,9 @@ type productVariables struct {
 	DeviceKernelHeaders []string `json:",omitempty"`
 
 	BoardUsesQTIHardware *bool `json:",omitempty"`
+
+	// include Pure variables
+	*android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
